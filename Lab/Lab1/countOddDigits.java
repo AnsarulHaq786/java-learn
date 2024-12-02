511bf6b8001e930791722a0cede2f11900822a73
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-12-02 22:46:10 +0545

    count odd digits in a number

---
+++ Lab/Lab1/countOddDigits.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class number{
+    private int number;
+    public void setData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number");
+        number=sc.nextInt();
+    }
+    public int countOdd(){
+        int count=0;
+        int ext;
+        while(number!=0){
+            ext=number%10;
+            if(ext%2!=0)
+                count++;
+            number=number/10;
+        }
+        return count;
+    }
+}
+class countOddDigits {
+    public static void main(String [] args){
+        number n=new number();
+        n.setData();
+        int count=n.countOdd();
+        System.out.println("Odd digits count="+count);
+    }
+}
