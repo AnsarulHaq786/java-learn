a3325ae5b22127305c797390c6969684890b63c7
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-12-03 23:46:13 +0545

    checks if string is palindrome or not

---
+++ Lab/Lab1/stringPalindrome.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class palindrome{
+    private String str;
+    public void setData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        str=sc.nextLine();
+    }
+    public boolean checkPalindrome(){
+        String revStr = "";
+        for(int i=str.length()-1; i>=0; i--){
+            char ext = str.charAt(i);
+            revStr=revStr+ext;
+        }
+        if(str.equalsIgnoreCase(revStr))
+            return true;
+        else
+            return false;
+    }
+}
+class stringPalindrome{
+    public static void main(String [] args){
+        palindrome p = new palindrome();
+        p.setData();
+        boolean check=p.checkPalindrome();
+        if(check)
+            System.out.println("Palindrome!");
+        else
+            System.out.println("Not Palindrome!");
+    }
+}
