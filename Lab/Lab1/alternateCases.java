f9e861205517bae4e07f2e726bf2737c20d94c5f
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-12-03 23:45:33 +0545

    alternate cases of string

---
+++ Lab/Lab1/alternateCases.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class cases{
+    private String str;
+    public void setData(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any string:");
+        str=sc.nextLine();
+    }
+    public String caseAlternator(){
+        String newStr="";
+        for(int i=0; i<str.length(); i++){
+            char ext=str.charAt(i);
+            if(i%2==0)
+                newStr=newStr+Character.toUpperCase(ext);
+            else
+                newStr=newStr+Character.toLowerCase(ext);
+        }
+        return newStr;
+    }
+}
+class alternateCases{
+    public static void main(String [] args){
+        cases c=new cases();
+        c.setData();
+        String res=c.caseAlternator();
+        System.out.println(res);
+    }
+}
